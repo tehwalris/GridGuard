c7c7071a523197b1f0e9005ff51793448539f4b8
Marc <nxt191@gmail.com>
2022-09-16 21:57:39 +0200

    basic setup power-client

--- power-client/src/App.tsx
+++ power-client/src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Admin from "./Admin/Admin";
+import User from "./User/User";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/user" element={<User />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route
+          path="/"
+          element={
+            <ul>
+              <li>
+                <Link to="/user">User</Link>
+              </li>
+              <li>
+                <Link to="/admin">Admin</Link>
+              </li>
+            </ul>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
